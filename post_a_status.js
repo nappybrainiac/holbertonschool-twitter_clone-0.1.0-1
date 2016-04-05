652d61e5a120768552e9efa17ce9f00e7220b029
Gloria Bwandungi <30@holbertonschool.com>
2016-04-05 14:54:08 -0400

    web performance

--- post_a_status.js
+++ post_a_status.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() { 
-	document.getElementById("post-status-btn").addEventListener("click", postpre);
-	function postpre() { // call toggle function on poststatusbtn
-		var elem = document.getElementById('statuspost');
-		elem.toggle(); // call toggle on div containing new status post
-		var elem = document.getElementById('post-status-btn');
-		elem.toggle();
-	}
-});
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("post-status-btn").addEventListener("click",postpre);function postpre(){var elem=document.getElementById('statuspost');elem.toggle();var elem=document.getElementById('post-status-btn');elem.toggle();}});
--- toggle.js
+++ toggle.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() { // wait til page is loaded
-	HTMLElement.prototype.toggle = function() { 
-	   if (this.classList.contains("hidden")) { // if element is hidden, remove hidden.
-	    this.classList.remove('hidden');
-	  } else {
-	    this.classList.add('hidden'); // else add hidden
-	  }
-	}
-});
+document.addEventListener("DOMContentLoaded",function(){HTMLElement.prototype.toggle=function(){if(this.classList.contains("hidden")){this.classList.remove('hidden');}else{this.classList.add('hidden');}}});
